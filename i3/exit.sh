383b5141efe2b0d0b2d748709d7e1cee769ff145
Pine <77921296+PineTreePizza@users.noreply.github.com>
2022-11-02 20:43:43 +0300

    Main Configs
    These are the main configs on my machine

---
+++ i3/exit.sh
@@ -0,0 +1,13 @@
+i3-resurrect save -w "1"
+i3-resurrect save -w "2"
+i3-resurrect save -w "3"
+i3-resurrect save -w "4"
+i3-resurrect save -w "5"
+i3-resurrect save -w "6"
+i3-resurrect save -w "7"
+i3-resurrect save -w "8"
+i3-resurrect save -w "9"
+i3-resurrect save -w "10"
+sleep 2
+i3 exit
+exit
